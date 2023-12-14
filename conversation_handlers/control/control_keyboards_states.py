c9a7cc44e3caf70ea224a957300289ebe26fab46
cardosofede <federico.cardoso.e@gmail.com>
2023-12-14 20:16:21 -0300

    (feat) adapt keyboards

--- conversation_handlers/control/control_keyboards_states.py
+++ conversation_handlers/control/control_keyboards_states.py
@@ -1,7 +1,9 @@
 from telegram import ReplyKeyboardMarkup
 
 # Define command stages for the 'Control' conversation
-CHOOSE_CONTROL_TASK, REVIEW_OVERDUE, REVIEW_TODAY, EDIT_TASK, DELETE_TASK, ANALYZE_CONTROL = map(chr, range(6))
+CHOOSE_CONTROL_TASK = map(chr, range(1))
+
+REVIEW_OVERDUE, REVIEW_TODAY, EDIT_TASK, DELETE_TASK, ANALYZE_CONTROL = map(chr, range(5))
 
 # Define keyboard for the 'Control' conversation
 control_keyboard = ReplyKeyboardMarkup(
--- conversation_handlers/plan/schedule_tasks/schedule_keyboards_states.py
+++
@@ -1,6 +0,0 @@
-from telegram import ReplyKeyboardMarkup
-
-schedule_keyboard = ReplyKeyboardMarkup(
-        [["Today", "Tomorrow"], ["Next week", "Custom date"], ["Skip", "Stop Scheduling"]],
-        one_time_keyboard=True
-    )
