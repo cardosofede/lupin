58af8ba1017ce7833e297f0cb96c931b76d85019
cardosofede <federico.cardoso.e@gmail.com>
2023-12-16 15:05:36 -0300

    (feat) apply autoflake and linter to all files

--- conversation_handlers/common_handlers.py
+++ conversation_handlers/common_handlers.py
@@ -6,5 +6,7 @@ from main_keyboards_states import BACK_TO_MAIN_MENU, main_menu_keyboard
 
 async def back_to_main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
     """Transition back to the main menu."""
-    await update.message.reply_text("⏎ Returning to the main menu...", reply_markup=main_menu_keyboard)
+    await update.message.reply_text(
+        "⏎ Returning to the main menu...", reply_markup=main_menu_keyboard
+    )
     return BACK_TO_MAIN_MENU
