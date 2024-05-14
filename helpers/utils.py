3f94a6d912755f96bac2b6bef94f5a1af4c0e45d
cardosofede <federico.cardoso.e@gmail.com>
2024-05-13 23:19:23 -0500

    (feat) remove lupin tg bot

--- helpers/utils.py
+++
@@ -1,7 +0,0 @@
-from typing import Dict
-
-
-def facts_to_str(user_data: Dict[str, str]) -> str:
-    """Format the gathered user info."""
-    facts = [f"{key} - {value}" for key, value in user_data.items()]
-    return "\n".join(facts).join(["\n", "\n"])
